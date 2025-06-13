77f6935d57b24366169c8380b94b499802e46d7b
zaggy2188 <zane202130@student-schoolsofwestfield.org>
2025-06-13 14:05:14 +0000

    not done

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function setup(){
+let ok = document.createElement("button");
+ok.innerHTML="OK";
+let buttons = document.getElementById("buttons");
+buttons.appendChild(ok)
+
 // 7 lines of code, including (not in this order)
 // appendChild
 // createElement
 // getElementByID
 // innerHTML
-}
+
