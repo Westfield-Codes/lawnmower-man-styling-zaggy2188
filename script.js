0b79e6aedff48e346bd2a9f5c8826d0c07606d17
zaggy2188 <zane202130@student-schoolsofwestfield.org>
2025-06-16 13:47:18 +0000

    done

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ ok.innerHTML="OK";
 let buttons = document.getElementById("buttons");
 buttons.appendChild(ok)
 
+let cancel = document.createElement("button");
+cancel.innerHTML="Cancel";
+buttons.appendChild(cancel)
 // 7 lines of code, including (not in this order)
 // appendChild
 // createElement
